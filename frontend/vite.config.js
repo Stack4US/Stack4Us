b21547bd04b6f842b889757dda11a99214546828
Anarragan <111092169+Anarragan@users.noreply.github.com>
2025-09-01 13:39:49 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // 👈 asegura que en producción los assets se carguen desde la raíz
+  base: '/', 
   server: {
-    host: true,     // escucha en 0.0.0.0 para acceder desde la red / ngrok
+    host: true,     
     port: 5173,
     allowedHosts: true // permite cualquier host (incluye subdominios ngrok)
     // Si prefieres restringir explícitamente usa, por ejemplo:
